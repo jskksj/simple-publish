eea71641c19f55c676ec06c8afc2e2ded70d0ae7
John Krumpotick <softwaredevelopment@simplexapparati.com>
2017-12-20 20:29:28 -0600

    Copy CSS to published folder

--- publish-project.el
+++ publish-project.el
@@ -2,23 +2,37 @@
 (require 'org)
 (require 'ox)
 (require 'ox-publish)
+(setq debug-on-error t)
 
-(setq org-publish-project-alist '(("simple-publish"
-                                   :author "Your Name Here"
-                                   :with-email "who@where"
-                                   :base-directory "./org"
-                                   :base-extension "org"                         ;; Only process org-mode files.
-                                   :publishing-directory "./published"
-                                   :publishing-function org-html-publish-to-html
-                                   :section-numbers nil
-                                   :with-toc nil
-                                   :time-stamp-file nil
-                                   :html-extension "html"
-                                   :html-head-include-scripts nil                ;; Do not include the default javascript.
-                                   :html-head-include-default-style nil          ;; Do not include the default css styles.
-                                   :html-html5-fancy t                           ;; Supposedly this is required for HTML5 output.
-                                   :html-doctype "html5"                         ;; And yes, render out HTML5.
-                                   :html-preamble nil
-                                   :html-postamble nil
-                                   :exclude "publish-[a-z]+.org"                 ;; Files prefixed with 'publish-' should not be exported.
-                                   :recursive t)))
+(setq org-publish-project-alist
+    '(
+      ;; org files to be exported to HTML.
+      ("simple-publish-org"
+       :author "Your Name Here"
+       :with-email "who@where"
+       :base-directory "./org"
+       :base-extension "org"
+       :publishing-directory "./published"
+       :publishing-function org-html-publish-to-html
+       :section-numbers nil
+       :with-toc nil
+       :time-stamp-file nil
+       :html-extension "html"
+       :html-head-include-scripts nil
+       :html-head-include-default-style nil
+       :html-html5-fancy t
+       :html-doctype "html5"
+       :html-preamble nil
+       :html-postamble nil
+       :exclude "publish-[a-z]+.org"
+       :recursive t)
+
+      ;; CSS files to be copied to the published folder.
+      ("simple-publish-css"
+       :base-directory "./org/css"
+       :base-extension "css"
+       :publishing-directory "./published/css"
+       :publishing-function org-publish-attachment)
+      ("simple-publish" :components ("simple-publish-org" "simple-publish-css"))
+    )
+)
